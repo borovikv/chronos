34e3e9df988ffca3c69f4d07612f7d4b51ee0701
vladimir <vladimir.borovic@gmail.com>
2016-01-04 10:44:42 +0200

    refactoring

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def div_with_error_class_if(condition, attributes = {}, &block)
+
+    if condition
+      div_class = attributes[:class]
+      attributes[:class] = (div_class && (div_class + ' ') || '') + 'has-error'
+    end
+    content_tag('div', attributes, &block)
+  end
 end
