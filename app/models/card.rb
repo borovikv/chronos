8fe6e0ca1be23142f7d9be8764d0fee6d6c5eae4
vladimir <vladimir.borovic@gmail.com>
2016-01-15 15:28:05 +0200

    Edge

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,49 @@
 class Card < ActiveRecord::Base
   belongs_to :group
   has_many :comments
+  has_many :a_edges, class_name: 'Edge', foreign_key: :card_a_id
+  has_many :b_edges, class_name: 'Edge', foreign_key: :card_b_id
 
   def to_s
     title
   end
 
+  def parent
+    begin
+      edge = Edge.where(card_b_id: id, relation: EdgesController.helpers.parent).first
+      edge.card_a
+    rescue
+      nil
+    end
+
+  end
+
+  def children
+    Card.joins(:a_edges).where("edges.card_a_id = #{id} and edges.relation = #{ EdgesController.helpers.parent }")
+  end
+
+  def related_cards
+    Card.joins(:a_edges).joins(:b_edges)
+        .where("(edges.card_a_id = #{id} or edges.card_b_id = #{id}) " +
+                   "and edges.relation != #{ EdgesController.helpers.parent } " +
+                   "and cards.id != #{id}"
+        ).distinct
+  end
+
+  def directed_relation_cards
+    Card.joins(:a_edges).joins(:b_edges)
+        .where("(edges.card_a_id = #{id} or edges.card_b_id = #{id}) " +
+                   "and edges.relation == #{ EdgesController.helpers.directed } " +
+                   "and cards.id != #{id}"
+        ).distinct
+  end
+
+  def undirected_relation_cards
+    Card.joins(:a_edges).joins(:b_edges)
+        .where("(edges.card_a_id = #{id} or edges.card_b_id = #{id}) " +
+                   "and edges.relation == #{ EdgesController.helpers.undirected } " +
+                   "and cards.id != #{id}"
+        ).distinct
+  end
 
 end
--- app/models/edge.rb
+++ app/models/edge.rb
@@ -2,4 +2,32 @@ class Edge < ActiveRecord::Base
   belongs_to :card_a, class_name: 'Card', foreign_key: :card_a_id
   belongs_to :card_b, class_name: 'Card', foreign_key: :card_b_id
 
+  validates :card_a_id, :card_b_id, :relation, presence: true, on: :create
+  validate :has_cards, on: :create
+  validate :only_one_parent, on: :create
+  private
+
+  def has_cards
+    errors.add(:card_a_id, "card doesn't exist") unless Card.exists? card_a_id
+    errors.add(:card_b_id, "card doesn't exist") unless Card.exists? card_b_id
+  end
+
+  def only_one_parent
+    if relation == EdgesController.helpers.parent
+      if Edge.exists?(card_b_id: card_b_id, relation: relation)
+        errors.add(:card_b_id, "card #{card_b} already have parent")
+      end
+
+      if Edge.exists?(card_a_id: card_b_id, card_b_id: card_a_id, relation: relation)
+        errors.add(:card_a_id, " [#{card_a}] can't have as child self parent [#{card_b}]")
+      end
+
+      if card_a_id == card_b_id
+        errors.add(:card_a_id, 'card can be self parent')
+        errors.add(:card_b_id, 'card can be self parent')
+      end
+    end
+
+  end
+
 end
