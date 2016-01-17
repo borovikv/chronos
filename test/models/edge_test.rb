53d47ac989280c41240a258a579c35869cbbb353
vladimir <vladimir.borovic@gmail.com>
2016-01-17 13:00:22 +0200

    added edge test dummy

--- test/models/edge_test.rb
+++ test/models/edge_test.rb
@@ -1,7 +1,27 @@
 require 'test_helper'
 
 class EdgeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should not have two parents' do
+    assert false
+    end
+
+  test 'should not have itself as parent' do
+    assert false
+  end
+
+  test 'should not have self child as parent' do
+    assert false
+  end
+
+
+  test 'should create edge' do
+    assert false
+  end
+
+  test 'should not create edge to itself' do
+    assert false
+  end
+
+
 end
